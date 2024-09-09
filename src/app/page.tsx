8f3df077f010e0ca74f3e5b449867bcaa005969d
Merlin <contact@merlin.ac>
2024-09-09 23:37:53 +0200

    style: showing home lines on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,18 +24,18 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl font-bold text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text uppercase"
+      <h1 className="py-3.5 px-0.5 z-10 text-7xl font-bold text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text uppercase"
       // style={{transform: "scaleY(1.1)"}}
       >
         Merlin
       </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in flex flex-col items-center">
         <h2 className="text-lg text-zinc-500 font-normal hover:text-zinc-200 ease-in-out transition-colors duration-500 relative glitch">
           FULL STACK DEVELOPER
