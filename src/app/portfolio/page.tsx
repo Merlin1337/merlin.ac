bb5c1ae8ad09bdd9417535f886601ee3f18fadb2
Merlin <contact@merlin.ac>
2024-09-09 23:34:16 +0200

    Changing portfolio page subtitle + responsive

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -10,10 +10,10 @@ export default function ProjectsPage() {
             <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
                 <div className="max-w-2xl mx-auto lg:mx-0">
                     <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-                        Projects
+                        Portfolio
                     </h2>
                     <p className="mt-4 text-zinc-400">
-                        Some of the projects are from work and some are on my own time.
+                        From concept to deployment, explore my development projects that address real-world needs with elegance and efficiency.
                     </p>
                 </div>
                 <div className="w-full h-px bg-zinc-800" />
@@ -32,7 +32,7 @@ export default function ProjectsPage() {
                         </Link>
 
                         {/* Technologies Used */}
-                        <div className="flex justify-center space-x-2 mt-2 text-zinc-400">
+                        <div className="flex flex-wrap justify-center gap-2 mt-2 text-zinc-400">
                             <span className="px-3 py-1 bg-zinc-800 rounded-full">React Native</span>
                             <span className="px-3 py-1 bg-zinc-800 rounded-full">NestJS</span>
                             <span className="px-3 py-1 bg-zinc-800 rounded-full">KafkaJS</span>
@@ -44,7 +44,7 @@ export default function ProjectsPage() {
                     <div>
                         <Link href="https://spotilabs.fr">
                             <div className="relative overflow-hidden rounded-2xl w-full h-[30rem] transition-transform ease-in-out duration-500 hover:scale-95">
-                            <Image width={592} height={480} src="/spotilabs_mockup.webp" className="overflow-hidden transition-transform ease-in-out duration-500 hover:scale-110 object-cover w-full h-full" alt="Spotilabs Website" />
+                                <Image width={592} height={480} src="/spotilabs_mockup.webp" className="overflow-hidden transition-transform ease-in-out duration-500 hover:scale-110 object-cover w-full h-full" alt="Spotilabs Website" />
                             </div>
                             <div className="flex flex-col justify-center items-center mt-4">
                                 <div className="font-medium">Spotilabs</div>
@@ -53,7 +53,7 @@ export default function ProjectsPage() {
                         </Link>
 
                         {/* Technologies Used */}
-                        <div className="flex justify-center space-x-2 mt-2 text-zinc-400">
+                        <div className="flex flex-wrap justify-center gap-2 mt-2 text-zinc-400">
                             <span className="px-3 py-1 bg-zinc-800 rounded-full">React</span>
                             <span className="px-3 py-1 bg-zinc-800 rounded-full">Next.js</span>
                             <span className="px-3 py-1 bg-zinc-800 rounded-full">PostgreSQL</span>
@@ -61,7 +61,6 @@ export default function ProjectsPage() {
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
